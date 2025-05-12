a30ec0584f76aef11ae49004a1559b944e49e4e6
Mohamad Muwfak Hlal <mmhl@tec.dk>
2025-05-12 11:33:00 +0200

    add encryption to progam

--- Program.cs
+++ Program.cs
@@ -1,88 +1,106 @@
-﻿using System;
+﻿// ============================
+// SERVER SIDE (Hybrid Encryption)
+// ============================
+
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 
-namespace TcpMultiClientServer
+class SecureServer
 {
-    class Program
-    {
-        private static List<TcpClient> clients = new List<TcpClient>();
-        private static readonly object clientLock = new object();
+    static List<TcpClient> clients = new List<TcpClient>();
+    static object clientLock = new object();
+    static RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048);
+    static Dictionary<TcpClient, byte[]> aesKeys = new Dictionary<TcpClient, byte[]>();
 
-        static void Main(string[] args)
+    static void Main()
+    {
+        int port = 5000;
+        TcpListener server = new TcpListener(IPAddress.Any, port);
+        server.Start();
+        Console.WriteLine("Server started.");
+
+        while (true)
         {
-            int port = 5000;
-            TcpListener server = new TcpListener(IPAddress.Any, port);
-            server.Start();
-            Console.WriteLine($"Server started on port {port}");
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+            lock (clientLock) clients.Add(client);
+            new Thread(HandleClient).Start(client);
+        }
+    }
+
+    static void HandleClient(object clientObj)
+    {
+        TcpClient client = (TcpClient)clientObj;
+        NetworkStream stream = client.GetStream();
+        byte[] buffer = new byte[2048];
+
+        try
+        {
+            // Step 1: Send RSA public key to client
+            string publicKey = rsa.ToXmlString(false);
+            byte[] publicKeyBytes = Encoding.UTF8.GetBytes(publicKey);
+            stream.Write(publicKeyBytes, 0, publicKeyBytes.Length);
+
+            // Step 2: Receive encrypted AES key from client
+            int aesKeyLength = stream.Read(buffer, 0, buffer.Length);
+            byte[] encryptedAesKey = new byte[aesKeyLength];
+            Array.Copy(buffer, encryptedAesKey, aesKeyLength);
+            byte[] aesKey = rsa.Decrypt(encryptedAesKey, false);
+            aesKeys[client] = aesKey;
 
             while (true)
             {
-                TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("Client connected");
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0) break;
 
-                lock (clientLock)
+                byte[] encryptedMsg = new byte[bytesRead];
+                Array.Copy(buffer, encryptedMsg, bytesRead);
+
+                using Aes aes = Aes.Create();
+                aes.Key = aesKey;
+                aes.IV = new byte[16];
+                ICryptoTransform decryptor = aes.CreateDecryptor();
+                string message = Encoding.UTF8.GetString(decryptor.TransformFinalBlock(encryptedMsg, 0, encryptedMsg.Length));
+
+                Console.WriteLine("Received: " + message);
+
+                // Broadcast encrypted message to all clients
+                byte[] plainData = Encoding.UTF8.GetBytes(message);
+                foreach (TcpClient otherClient in clients.ToArray())
                 {
-                    clients.Add(client);
-                }
-
-                Thread clientThread = new Thread(HandleClient);
-                clientThread.Start(client);
-            }
-        }
-
-        static void HandleClient(object clientObj)
-        {
-            TcpClient client = (TcpClient)clientObj;
-            NetworkStream stream = client.GetStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-
-            try
-            {
-                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine($"Received: {message}");
-
-                    // Prepare broadcast message
-                    byte[] broadcastData = Encoding.UTF8.GetBytes(message);
-
-                    // Broadcast to all clients
-                    lock (clientLock)
+                    try
                     {
-                        foreach (TcpClient otherClient in clients.ToArray())
-                        {
-                            try
-                            {
-                                NetworkStream otherStream = otherClient.GetStream();
-                                otherStream.Write(broadcastData, 0, broadcastData.Length);
-                            }
-                            catch
-                            {
-                                Console.WriteLine("Removing dead client.");
-                                clients.Remove(otherClient);
-                            }
-                        }
+                        Aes broadcastAes = Aes.Create();
+                        broadcastAes.Key = aesKeys[otherClient];
+                        broadcastAes.IV = new byte[16];
+                        ICryptoTransform encryptor = broadcastAes.CreateEncryptor();
+                        byte[] encryptedData = encryptor.TransformFinalBlock(plainData, 0, plainData.Length);
+                        otherClient.GetStream().Write(encryptedData, 0, encryptedData.Length);
+                    }
+                    catch
+                    {
+                        clients.Remove(otherClient);
                     }
                 }
             }
-            catch (Exception ex)
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Client error: " + ex.Message);
+        }
+        finally
+        {
+            stream.Close();
+            client.Close();
+            lock (clientLock)
             {
-                Console.WriteLine("Client connection error: " + ex.Message);
-            }
-            finally
-            {
-                lock (clientLock)
-                {
-                    clients.Remove(client);
-                }
-                stream.Close();
-                client.Close();
-                Console.WriteLine("Client disconnected.");
+                clients.Remove(client);
+                aesKeys.Remove(client);
             }
         }
     }
